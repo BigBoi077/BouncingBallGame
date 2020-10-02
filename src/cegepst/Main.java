a407533d4d183e51e18e580c3da4d3671ee969c5
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2020-10-02 16:29:01 -0400

    Initial commit

---
+++ src/cegepst/Main.java
@@ -0,0 +1,11 @@
+package cegepst;
+
+import cegepst.engine.GameWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.start();
+    }
+}
