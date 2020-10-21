d1b90c216334a88248f4fdd8508e43c42b52b30d
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2020-10-21 16:34:02 -0400

    Starting new game

--- src/cegepst/Main.java
+++ src/cegepst/Main.java
@@ -1,11 +1,8 @@
 package cegepst;
 
-import cegepst.engine.BouncingBallGame;
-
 public class Main {
 
     public static void main(String[] args) {
-        BouncingBallGame game = new BouncingBallGame();
-        game.start();
+        
     }
 }
